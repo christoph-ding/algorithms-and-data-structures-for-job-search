3b00057e8f79c8088a49f77fb61203018008e252
ironpup <christoph.ding@gmail.com>
2018-03-03 12:59:59 -0500

    Being reverse words using no extra space (do the reverse in place), stub out the steps.

---
+++ random-practice-by-week/3.1.18/reverseWords.js
@@ -0,0 +1,30 @@
+function reverseWords(sentence) {
+  // we find the next space so that we have the bounds of a 'word'
+  let start,
+      end;
+
+  // keep doing this while there are more words 
+  // reverse that word
+
+
+}
+
+function reverseSingleWord(sentence, start, end) {
+  // find the middle of the word
+  // iterate backwards from the end to the middle
+  // iterate forwards from the start to the middle
+  // for each, swap the two ... use temp variables, blah blah 
+
+}
+
+// the goal:
+// linear time
+// constant space:
+// constant number of 'helper' variables
+// chance IN PLACE for the ... most interesting problem
+
+// DON"T USE:
+// extra space (no arrays)
+
+// test
+reverseWords('hello world')
