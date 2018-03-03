c34322344415fc559f545cba8820cf6b8cfbdd20
ironpup <christoph.ding@gmail.com>
2018-03-03 14:25:01 -0500

    Had to do soooo much refactoring to account for fact that there are no character arrays in javascript.

--- random-practice-by-week/3.1.18/reverseWords.js
+++ random-practice-by-week/3.1.18/reverseWords.js
@@ -6,45 +6,30 @@ function reverseWords(sentence) {
 
   // keep doing this while there are more words 
   // reverse that word
-
 }
 
-function reverseSingleWord(sentence, start, middle, end) {
+function reverseSingleWord(word) {
   // unfortunately we cannot change the characters of a string in place
-
-
-  start = start || 0
-  end = end || sentence.length - 1
-  middle = middle || Math.floor((start + end) / 2)
-
-  console.log('middle: ', middle, ' letter: ', sentence[middle])
-
-  let leftLetter,
-      rightLetter;
-
-
+  let wordAsCharacterArray = makeCharArray(word)
+  let middle = Math.floor((word.length) / 2)
 
   // iterate backwards from the end to the middle
   // iterate forwards from the start to the middle
   // for each, swap the two ... use temp variables, blah blah 
-  for (let i = start, j = end; i <= middle && j >= middle; i++, j--) {
-    leftLetter = sentence[i]
-    rightLetter = sentence[j]
-    console.log('leftLetter: ', leftLetter, ' rightLetter: ', rightLetter)
-    swap(sentence, i, j)
-    console.log('now: ', sentence)
+  for (let i = 0, j = word.length - 1; i <= middle && j >= middle; i++, j--) {
+    swap(wordAsCharacterArray, i, j)
   }
+  return wordAsCharacterArray.join('')
 }
 
-function makeCharArray(sentence, start, end) {
-  
+function makeCharArray(word) {
+  return word.slice().split('')
 }
 
-function swap(sentence, indexOne, indexTwo) {
-  let tempRight = sentence[indexTwo]
-  console.log('tempRight: ', tempRight)
-  sentence[indexTwo] = sentence[indexOne]
-  sentence[indexOne] = tempRight
+function swap(word, indexOne, indexTwo) {
+  let tempRight = word[indexTwo]
+  word[indexTwo] = word[indexOne]
+  word[indexOne] = tempRight
 }
 
 // the goal:
@@ -57,5 +42,4 @@ function swap(sentence, indexOne, indexTwo) {
 // extra space (no arrays)
 
 // test
-reverseSingleWord('robot') // middle is index 2 <- don't swap '2'
-// reverseSingleWord('robots') // middle is index 3 <- swap '2' and '3'
+console.log(reverseSingleWord('robxt'))
