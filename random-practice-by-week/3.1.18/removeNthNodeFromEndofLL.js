850719860466ef93dff350ab14d8b3d345c7e35c
ironpup <christoph.ding@gmail.com>
2018-03-03 10:29:13 -0500

    Implement a linked list runner and finding where the fast runner should 'start'.

--- random-practice-by-week/3.1.18/removeNthNodeFromEndofLL.js
+++ random-practice-by-week/3.1.18/removeNthNodeFromEndofLL.js
@@ -12,7 +12,14 @@ function removeNthNodefromEndofLL(headNode, Nth) {
   let counter = 0
 
   // while loop to put the faster runner ahead
+  while (counter < Nth && fastRunner !== null) {
+    console.log('fastRunner: ', fastRunner.key, ' counter: ', counter )
+    // what if the list is shorter than 'Nth'?
+    fastRunner = fastRunner.next
+    counter++
+  }
 
+  console.log('fast runner starting at: ', fastRunner.key)
 
   // run through the linked list until the fastRunner hits the end
 
@@ -22,4 +29,28 @@ function removeNthNodefromEndofLL(headNode, Nth) {
 
 }
 
+function iterateThroughLL(headOfLinkedList) {
+  let current = headOfLinkedList
+  const vals = []
+  while (current) {
+    vals.push(current.key)
+    current = current.next
+  }
+  console.log(vals.join( ' -> '))
+
+}
+
 // test
+let three = new linkedListNode(3)
+let two = new linkedListNode(2, three)
+let one = new linkedListNode(1, two)
+
+iterateThroughLL(one)
+removeNthNodefromEndofLL(one, 2)
+
+// 1 2 3 
+// 2nd from last - remove 2
+// slow fast
+//  2   null
+//  1     3
+// so fast is 2 (N) ahead of slow
