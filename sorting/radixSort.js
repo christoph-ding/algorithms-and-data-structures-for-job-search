05f3ae5ac57d4f831b05ac33fbd37c73fe096820
ironpup <christoph.ding@gmail.com>
2018-03-06 22:29:30 -0500

    Implement a helper function that normalizes the elements in the list so that all elements have the same number of digits, using leading zeroes.

--- sorting/radixSort.js
+++ sorting/radixSort.js
@@ -1,7 +1,18 @@
 function radixSort(list) {
   // generate bins + helper queue
+  let bins = generateBins()
+  let helperQueue = list.slice()
 
-  let helperQueue = []
+  console.log('bins: ', bins) 
+  
+
+  // 'normalize' the number of digits in each
+  let maxDigits = findMostDigits()
+  normalize(helperQueue, maxDigits)
+
+  console.log('max digits: ', maxDigits)
+  console.log('helperQueue: ', helperQueue)
+  
 
   // for each 'digit's place' in the element in key with the most digits
     // put each element, using it's 'digits place', put into proper 'bin', maintaing order
@@ -10,10 +21,46 @@ function radixSort(list) {
 
     // after finishing sorting into bins, requeue into helperQueue
 
+
+  return helperQueue
 }
 
 function generateBins() {
+  let bins = {}
 
+  for (let i = 0; i <= 9; i++) {
+    bins[i] = []
+  }
+
+  return bins
+}
+
+function findMostDigits(list) {
+  let maxDigits = 0
+  let currentDigits = 0
+  list.forEach(function(element) {
+    currentDigits = String(element).length
+    if (currentDigits > maxDigits) {
+      maxDigits = currentDigits
+    }
+  })
+  return maxDigits
+}
+
+function normalize(list, digits) {
+  let elementAsString,
+      elementLength,
+      leadingZeroes,
+      normalizedElement
+
+  list.forEach((element, index) => {
+    elementAsString = String(element)
+    elementLength = elementAsString.length
+    leadingZeroes = new Array(digits - elementLength).join('0')
+    normalizedElement = leadingZeroes.concat(elementAsString)
+    console.log('element: ', element, ' normalizedElement: ', normalizedElement)
+    list[index] = normalizedElement
+  })
 }
 
 function sortIntoBins(bins, list) {
@@ -25,3 +72,6 @@ function requeue(bins, helperQueue) {
 }
 
 // tests
+let list = [100, 91, 999, 20, 7, 543, 1293, 76, 823, 3918]
+normalize(list, 10)
+console.log(list)
