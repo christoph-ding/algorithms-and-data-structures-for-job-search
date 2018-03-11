292ea4c19bd0284f5172fc4973319f4bd39afdd7
ironpup <christoph.ding@gmail.com>
2018-03-11 16:57:14 -0400

    Implement and test one away.

--- random-practice-by-week/3.8.18/oneAway.js
+++ random-practice-by-week/3.8.18/oneAway.js
@@ -1,5 +1,4 @@
 function oneAway(one, two) {
-
   let discrepencies = 0
 
   let oneRunner = 0
@@ -7,21 +6,37 @@ function oneAway(one, two) {
   let oneChar,
       twoChar;
 
-  while (oneRunner < one.length && twoRunner < two.length) {
+  while (oneRunner < one.length || twoRunner < two.length) {
     oneChar = one[oneRunner]
     twoChar = two[twoRunner]
     
-    if (oneChar !== twoChar && !(discrepencies)) {
-      console.log('mismatch!: ', 'oneChar: ', oneChar, ' twoChar: ', twoChar)
-    } else if (oneChar !== twoChar && discrepencies) {
+    // console.log('one: ', oneChar, ' two: ', twoChar, ' discrepencies: ', discrepencies)
+
+    if (oneChar !== twoChar) {
+      // we can 'shift' i.e. ale pale
+      if (one[oneRunner] === two[twoRunner + 1]) {
+        // console.log('shift one')
+        twoRunner++
+      } else if (one[oneRunner  + 1] === two[twoRunner]) { // pale ale
+        // console.log('shift two')
+        oneRunner++
+      }
+      discrepencies++
+    }
+    if (discrepencies > 1) {
       return false
     }
-
     // replace would work?
     oneRunner++
     twoRunner++
   }
+  return true
 }
 
 // tests
-console.log(oneAway('ale', 'ble')) // should return TRUE 
+console.log(oneAway('ace', 'ble')) // should return FALSE
+console.log(oneAway('a', 'b')) // TRUE
+console.log(oneAway('a', 'bc')) // FALSE
+console.log(oneAway('a', 'bcasdf')) // FALSE
+console.log(oneAway('ale', 'pale')) // TRUE
+console.log(oneAway('pale', 'ale')) // TRUE
