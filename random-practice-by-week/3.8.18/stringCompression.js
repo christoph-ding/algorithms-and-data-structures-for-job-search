d59bce33d3f9fd51cc30769b0366edd11a7d8ab5
ironpup <christoph.ding@gmail.com>
2018-03-17 14:31:34 -0400

    Continue to working on string compression.

--- random-practice-by-week/3.8.18/stringCompression.js
+++ random-practice-by-week/3.8.18/stringCompression.js
@@ -1,26 +1,44 @@
 function stringCompression(characters) {
-  let currentCharacterIndex = 0
-      currentCharacter
-      runnerIndex = 1
+  let currentCharacterIndex = 0,
+      currentCharacter = characters[currentCharacterIndex],
+      runnerIndex = currentCharacterIndex + 1,
+      differenceCurrentRunner = runnerIndex - currentCharacterIndex
       runnerCharacter;
 
-  while (currentRunnerIndex < characters.length) {
+  let countDigits = []
+
+  while (runnerIndex <= characters.length) {
     // update the runner character
+    runnerCharacter = characters[runnerIndex]
+
+    console.log('runnerCharacter: ', runnerCharacter,
+                'currentCharacter: ', currentCharacter,
+                'difference: ', differenceCurrentRunner)
+
     // are they different now? (current)
+    if (runnerCharacter !== currentCharacter) {
+      differenceCurrentRunner = runnerIndex - currentCharacterIndex
+      // what is the difference between 2 indices?
+      // if only 1, rules...
+      if (differenceCurrentRunner === 1) {
+        // no writes
+        // currentCharacterIndex advances 1
+      } else {
+      // if more 1, rules...
+      // move currentCharacterIndex according to 'difference'
+        countDigits = differenceCurrentRunner.string().split('')
+        console.log('countDigits: ', countDigits)
+        countDigits.forEach((digit) => {
+          characters[currentCharacterIndex++] = digit
+        }
+        console.log('characters: ', characters)
+      }
+      // currentCharacter updates to new currentCharacter (the one just 'found' by fast runner)
 
-    // what is the difference between 2 indices?
-        // if only 1, rules...
-          // no writes
-          // currentCharacterIndex advances 1
-        // if more 1, rules...
-          // write 'difference' to currentCharacterIndex + 1 ... or more cause digits
-          // move currentCharacterIndex according to 'difference'
-        // currentCharacter updates to new currentCharacter (the one just 'found' by fast runner)
-    // the runner will advance regardless
-  }
-
-
+    }
+    // the runner will advance regardless    
+    runnerIndex++
 }
 
 // tests
-console.log(stringCompression(['a', 'a', 'b', 'c', 'c'])) // [a, 2, b, c, 2]
+console.log(stringCompression(['a', 'a', 'b', 'a', 'c', 'c'])) // [a, 2, b, c, 2]
