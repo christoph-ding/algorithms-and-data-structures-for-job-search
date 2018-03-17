0026b4997913d3dc4b247bc48ba18cd1ada755a3
ironpup <christoph.ding@gmail.com>
2018-03-17 15:57:19 -0400

    Beging all permutations.

---
+++ random-practice-by-week/3.17.18/allPermutations.js
@@ -0,0 +1,17 @@
+function allPermutations(chars) {
+  let permutations = []
+  let currentPerm = []
+  let helperList = []
+
+  
+
+
+
+
+
+  return permutations
+} 
+
+// test
+let test = ['a', 'b', 'c']
+console.log(allPermutations(test))
