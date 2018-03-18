8cac8a602558cfb3d27fb4f323093a18a049e27c
ironpup <christoph.ding@gmail.com>
2018-03-18 12:40:50 -0400

    Begin implementing countAndSay.

---
+++ random-practice-by-week/3.17.18/countAndSay.js
@@ -0,0 +1,21 @@
+function countAndSay(n) {
+  let previous = [1]
+  let current = []
+  let round = 2
+
+  // helper variables for forming 'current' from 'previous'
+  let cur
+
+  while (round++ <= n) {
+    // update current
+
+
+    
+  }
+
+  return previous
+}
+
+// tests
+console.log(countAndSay(1)) // [1]
+// console.log(countAndSay(2)) // [1, 1]
