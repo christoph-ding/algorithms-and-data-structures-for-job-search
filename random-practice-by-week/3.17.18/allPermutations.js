54839f653b80f098ac000da5bd76d7d70b74e6da
ironpup <christoph.ding@gmail.com>
2018-03-18 12:03:20 -0400

    Implement all Permutations using recursion and n exponential space.

--- random-practice-by-week/3.17.18/allPermutations.js
+++ random-practice-by-week/3.17.18/allPermutations.js
@@ -1,13 +1,22 @@
 function allPermutations(chars) {
   let permutations = []
   let currentPerm = []
-  let helperList = []
 
+  function inner(subChars) {
+    if (subChars.length) {
+      subChars.forEach((elem, index) => {
+        currentPerm.push(elem)
+        let subSlice = subChars.slice(0, index).concat(subChars.slice(index + 1))
+        inner(subSlice)
+        currentPerm.pop()
+      })
+    } else {
+      // 'done'
+      permutations.push(currentPerm.slice())
+    }
+  }
   
-
-
-
-
+  inner(chars)
 
   return permutations
 } 
