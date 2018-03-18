5ffd880b6c82657fbe921e3356e38349edbb85ba
ironpup <christoph.ding@gmail.com>
2018-03-18 18:29:52 -0400

    Implement count and say, now testing.

--- random-practice-by-week/3.17.18/countAndSay.js
+++ random-practice-by-week/3.17.18/countAndSay.js
@@ -7,19 +7,23 @@ function countAndSay(n) {
   let currentSought,
       runSize
 
-  while (round++ <= n) {
+  while (round <= n) {
     // update currentCountAndSay
     currentSought = previous[0]
     runSize = 0
 
+    console.log('round: ', round, ' previous: ', previous, ' sought: ', currentSought)
+
     previous.forEach((currentChar, index) => {
       if (currentChar !== currentSought) {
+        console.log('mismatch at index: ', index, ' currentChar: ', currentChar, ' sought: ', currentSought)
         currentCountAndSay.push(runSize)
         currentCountAndSay.push(currentSought)
         runSize = 1
       } else {
         runSize++
         if (index === previous.length - 1) {
+          console.log()
            currentCountAndSay.push(runSize)
            currentCountAndSay.push(currentSought)
         }
@@ -28,11 +32,14 @@ function countAndSay(n) {
 
     previous = currentCountAndSay
     currentCountAndSay = []
+    round++
   }
 
   return previous
 }
 
 // tests
-console.log(countAndSay(1)) // [1]
+// console.log(countAndSay(1)) // [1]
 // console.log(countAndSay(2)) // [1, 1]
+// console.log(countAndSay(3)) // [2, 1]
+console.log(countAndSay(4)) // [1, 2, 1, 1]
