bf0114f29117c1772159c59620a038ac9bf925f1
ironpup <christoph.ding@gmail.com>
2018-04-03 22:38:24 -0400

    Stub out count and say.

---
+++ random-practice-by-week/4.1.18/countAndSay.js
@@ -0,0 +1,26 @@
+function countAndSay(n) {
+  // we need to instantiate n
+  // current = [ ... ] 
+  // some counter ...
+  // while counter < n
+
+  // next is formFromList(current)
+  // set current to next
+
+}
+
+function formFromList(list) {
+  let output = []
+  let count = 0;
+  let currentChar = null;
+
+  // 1. iterate through the list, but going 'beyond': elem
+    // in case we dont have count or currentChar
+    // elem matches currentChar
+    // does not match ... or 'went over the edge'
+
+  // 2. we can use 'for each'
+  //    but then we need to something to the remaining 'current' and 'count'
+
+  return output
+}
